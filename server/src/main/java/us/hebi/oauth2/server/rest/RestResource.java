0312668741dd1b3dd10ba66bf4618a04a089b5f7
Florian Enner <florian@enner.org>
2018-03-21 17:04:12 +0100

    removed unused imports

--- server/src/main/java/us/hebi/oauth2/server/rest/RestResource.java
+++ server/src/main/java/us/hebi/oauth2/server/rest/RestResource.java
@@ -7,8 +7,6 @@ import com.github.scribejava.core.model.Verb;
 import com.github.scribejava.core.oauth.OAuth20Service;
 
 import javax.ejb.Stateless;
-import javax.json.Json;
-import javax.json.JsonObject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
